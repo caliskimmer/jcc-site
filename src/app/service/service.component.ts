e275bdffe76f9ded0278c5a81b86e5c19e99da88
caliskimmer <caliskimmer@gmail.com>
2018-07-28 19:55:50 -0700

    responsiveness improved, few features added, bug with alternating colors in ngFor

--- src/app/service/service.component.ts
+++ src/app/service/service.component.ts
@@ -1,15 +1,16 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-service',
   templateUrl: './service.component.html',
   styleUrls: ['./service.component.css']
 })
-export class ServiceComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+export class ServiceComponent {
+  services: Any = [{name: 'Wellness Care', id: 'wellness-care'},
+                   {name: 'Sports Chiro', id: 'sports-chiro'},
+                   {name: 'Prenatal Care', id: 'prenatal-care'},
+                   {name: 'Graston Technique', id: 'graston-technique'},
+                   {name: 'Rocktape', id: 'rocktape'},
+                   {name: 'Massage Therapy', id: 'massage-therapy'},
+                   {name: 'Senior Care', id: 'senior-care'}];
 }
