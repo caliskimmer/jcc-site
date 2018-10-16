231281648da5f9ef951cbdf4f1d271d5470aeaaa
caliskimmer <caliskimmer@gmail.com>
2018-10-15 17:55:23 -0700

    added rudimentary mobile menu presentation

--- src/app/main/main.component.ts
+++ src/app/main/main.component.ts
@@ -12,7 +12,6 @@ export class MainComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
-    console.log("TEST: " + this.router);
   }
 
 }
--- src/app/mobile-menu/mobile-menu.component.ts
+++ src/app/mobile-menu/mobile-menu.component.ts
@@ -1,13 +1,24 @@
-import { Component, Input, Renderer2 } from '@angular/core';
+import { Component, Input, Renderer2, OnChanges} from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
+import { popoverSections } from '../data/navigation-links.js';
+
 @Component({
   selector: 'app-mobile-menu',
   templateUrl: './mobile-menu.component.html',
   styleUrls: ['./mobile-menu.component.css']
 })
-export class MobileMenuComponent {
+export class MobileMenuComponent implements OnChanges{
   @Input() header:HeaderComponent;
-  constructor(private renderer: Renderer2) {}
+  menuItems:any[];
+  constructor(private renderer: Renderer2) {
+  }
+
+  ngOnChanges() {
+    this.menuItems = [];
+    for (let item of this.header.navList) {
+      this.menuItems.push({item: item, isCollapsed: true, children: popoverSections[item]});  
+    }
+  }
   
   close() {
     this.renderer.removeClass(document.body, 'modal-open');
