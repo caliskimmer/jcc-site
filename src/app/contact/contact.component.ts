c11c84e9ac84f4454bcf7caeee11faa2866190d6
Matas <mempakeris@me.com>
2019-06-13 18:18:35 -0700

    contact form implemented

---
+++ src/app/contact/contact.component.ts
@@ -0,0 +1,76 @@
+import { Component, Input, OnChanges, Renderer2} from '@angular/core';
+import { MainComponent } from '../main/main.component';
+import {FormBuilder, FormControl, Validators} from '@angular/forms';
+
+import { BookingService } from '../booking-service/booking-service.service';
+import {parsePhoneNumberFromString} from 'libphonenumber-js';
+
+@Component({
+    selector: 'app-contact',
+    templateUrl: './contact.component.html',
+    styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnChanges {
+    @Input() main: MainComponent;
+    @Input() isClosed: boolean;
+    sentForm = false;
+    isSubmitted = false;
+
+    contactForm = this.fb.group({
+        firstName: ['', Validators.required],
+        lastName: ['', Validators.required],
+        email: ['', [Validators.required, Validators.email]],
+        phoneNumber: ['', [Validators.required, this.phoneValidator]],
+        message: ['', Validators.required],
+        captcha: ['', Validators.required]
+    });
+
+    constructor(private renderer: Renderer2,
+                private fb: FormBuilder,
+                private bookingService: BookingService) {}
+
+    ngOnChanges() {
+        if (this.isClosed) {
+            this.sentForm = false;
+        }
+    }
+
+    close() {
+        this.main.toggleContactForm();
+        this.contactForm.reset();
+    }
+
+    submitForm() {
+        const formBody = {
+            firstName: this.contactForm.get('firstName').value,
+            lastName: this.contactForm.get('lastName').value,
+            email: this.contactForm.get('email').value,
+            phone: this.contactForm.get('phoneNumber').value,
+            message: this.contactForm.get('message').value,
+        };
+
+        this.bookingService.sendForm('contact', formBody).subscribe((response) => {
+            this.sentForm = true;
+            this.isSubmitted = false;
+        }), err => {
+            throw err;
+        };
+
+        this.isSubmitted = true;
+    }
+
+    private phoneValidator(control: FormControl) {
+        const invalid = {'invalidPhone': true};
+
+        if (!control.value) {
+            return invalid;
+        }
+
+        const parsedNumber = parsePhoneNumberFromString(control.value.toString(), 'US');
+        if (!parsedNumber) {
+            return invalid;
+        }
+
+        return parsedNumber.isValid() ? null : invalid;
+    }
+}
