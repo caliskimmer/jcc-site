ed7a870225738d9cdcd5b6898a61bdf3e79fa59a
Matas <mempakeris@me.com>
2019-06-13 18:15:55 -0700

    work on admin login and home

---
+++ src/app/admin/admin.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { environment } from '../../environments/environment';
+import { Router } from '@angular/router';
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AdminService {
+
+    constructor(private http: HttpClient, private router: Router) { }
+
+    async login(formInput: any) {
+        try {
+            const result = await this.http.post(`${environment.api}/login`, formInput).toPromise();
+            const user = result['user'];
+
+            localStorage.setItem('token', user['token']);
+            this.router.navigate(['/admin']);
+            return true;
+        } catch (err) {
+            if (err.status === 403) {
+            } else {
+                console.log(`An error occurred submitting login form => ${err.message}`);
+            }
+
+            return false;
+        }
+    }
+}
