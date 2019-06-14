8744e79bae5bae286e0ed8728b49a0d7cb4d25bc
Matas <mempakeris@me.com>
2019-06-13 18:19:17 -0700

    more animation files

---
+++ src/app/main/main.animations.ts
@@ -0,0 +1,15 @@
+import { trigger, state, style, transition, animate } from '@angular/animations';
+
+export const animations = [
+  trigger('show-contact-form', [
+    state('contact-form-on', style({
+      visibility: 'visible',
+      opacity: 1
+    })),
+    state('contact-form-off', style({
+      visibility: 'hidden',
+      opacity: 0
+    })),
+    transition('* => *', animate('300ms linear'))
+  ]),
+];
