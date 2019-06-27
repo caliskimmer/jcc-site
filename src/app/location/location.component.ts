74fc2f83da74ef4dbdd5f679b4262ef82262f192
Matas <mempakeris@me.com>
2019-06-26 22:45:14 -0700

    map reactivated, routing change for service links to comply with routerlink style routing, tabbing reformat

--- src/app/location/location.component.ts
+++ src/app/location/location.component.ts
@@ -1,32 +1,33 @@
 import {Component, ViewChild, AfterViewInit, ElementRef, Renderer2} from '@angular/core';
 import { BookingService } from '../booking-service/booking-service.service';
+import {environment} from '../../environments/environment.prod';
 
 import * as L from 'mapbox.js';
 
 @Component({
-  selector: 'app-location',
-  templateUrl: './location.component.html',
-  styleUrls: ['./location.component.scss'],
+    selector: 'app-location',
+    templateUrl: './location.component.html',
+    styleUrls: ['./location.component.scss'],
 })
 export class LocationComponent implements AfterViewInit {
-  @ViewChild('map') map: ElementRef;
+    @ViewChild('map') map: ElementRef;
 
-  constructor(private renderer: Renderer2, private bookingService: BookingService) {
-    if (!this.bookingService.renderer) {
-      this.bookingService.renderer = renderer;
+    constructor(private renderer: Renderer2, private bookingService: BookingService) {
+        if (!this.bookingService.renderer) {
+            this.bookingService.renderer = renderer;
+        }
     }
-  }
 
-  ngAfterViewInit() {
-    const LONGITUDE = 33.955113;
-    const LATITUDE = -118.396457;
-    const ZOOM = 15;
-    L.mapbox.accessToken = 'temp'; // environment.mapbox.access_token;
-    const jccMap: any = L.mapbox.map(this.map.nativeElement, 'mapbox.streets').setView([LONGITUDE, LATITUDE], ZOOM);
-    L.marker([LONGITUDE, LATITUDE]).addTo(jccMap);
-  }
+    ngAfterViewInit() {
+        const LONGITUDE = 33.955113;
+        const LATITUDE = -118.396457;
+        const ZOOM = 15;
+        L.mapbox.accessToken = environment.mapbox.access_token;
+        const jccMap: any = L.mapbox.map(this.map.nativeElement, 'mapbox.streets').setView([LONGITUDE, LATITUDE], ZOOM);
+        L.marker([LONGITUDE, LATITUDE]).addTo(jccMap);
+    }
 
-  toggleBookingForm() {
-    this.bookingService.toggleBookingForm();
-  }
+    toggleBookingForm() {
+        this.bookingService.toggleBookingForm();
+    }
 }
