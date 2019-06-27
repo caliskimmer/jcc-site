a4de217c55a2254f0faf3c28a695de78d96c52d5
Matas <mempakeris@me.com>
2019-06-26 22:58:35 -0700

    tab changes, clicking link in mobile menu now closes it

--- src/app/mobile-menu/mobile-menu.component.ts
+++ src/app/mobile-menu/mobile-menu.component.ts
@@ -1,34 +1,42 @@
-import { Component, Input, Renderer2, OnChanges} from '@angular/core';
+import { Component, Input, Renderer2, OnChanges, OnInit } from '@angular/core';
 import { HeaderComponent } from '../header/header.component';
 import { popoverSections } from '../data/navigation-links.js';
 import { PopoverService } from '../popover-service/popover-service.service';
+import {Router} from '@angular/router';
 
 @Component({
-  selector: 'app-mobile-menu',
-  templateUrl: './mobile-menu.component.html',
-  styleUrls: ['./mobile-menu.component.scss']
+    selector: 'app-mobile-menu',
+    templateUrl: './mobile-menu.component.html',
+    styleUrls: ['./mobile-menu.component.scss']
 })
-export class MobileMenuComponent implements OnChanges{
-  @Input() header:HeaderComponent;
-  menuItems:any[];
+export class MobileMenuComponent implements OnChanges, OnInit {
+    @Input() header: HeaderComponent;
+    menuItems: any[];
 
-  constructor(private renderer: Renderer2,
-              private popoverService: PopoverService) {
-  }
-
-  ngOnChanges() {
-    this.menuItems = [];
-    for (let item of this.header.navList) {
-      this.menuItems.push({item: item, isCollapsed: true, children: popoverSections[item]});  
+    constructor(private renderer: Renderer2,
+                private popoverService: PopoverService,
+                private router: Router) {
     }
-  }
-  
-  close() {
-    this.renderer.removeClass(document.body, 'modal-open');
-    this.header.menuState = 'menu-off';
 
-    if (!this.header.detailPresent()) {
-      this.header.state = this.header.prevHeaderState;
+    ngOnInit() {
+        this.router.events.subscribe(() => {
+            this.close();
+        });
     }
-  }
+
+    ngOnChanges() {
+        this.menuItems = [];
+        for (const item of this.header.navList) {
+            this.menuItems.push({item: item, isCollapsed: true, children: popoverSections[item]});
+        }
+    }
+
+    close() {
+        this.renderer.removeClass(document.body, 'modal-open');
+        this.header.menuState = 'menu-off';
+
+        if (!this.header.detailPresent()) {
+            this.header.state = this.header.prevHeaderState;
+        }
+    }
 }
