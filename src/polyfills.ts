a07de2407f8ba3721c86039a509741d719df7f98
Matas <matasempakeris@gmail.com>
2020-03-22 16:17:33 -0700

    fixed problems with polyfill.js

--- src/polyfills.ts
+++ src/polyfills.ts
@@ -1,3 +1,7 @@
+/***************************************************************************************************
+ * Load `$localize` onto the global scope - used if i18n tags appear in Angular templates.
+ */
+import '@angular/localize/init';
 /**
  * This file includes polyfills needed by Angular and is loaded before the app.
  * You can add your own extra polyfills to this file.
@@ -42,7 +46,7 @@
 
 /** Evergreen browsers require these. **/
 // Used for reflect-metadata in JIT. If you use AOT (and only Angular decorators), you can remove.
-import 'core-js/es7/reflect';
+import 'core-js/es/reflect';
 /***************************************************************************************************
  * Zone JS is required by default for Angular itself.
  */
@@ -69,7 +73,6 @@ import 'zone.js/dist/zone'; // Included with Angular CLI.
  * with the following flag, it will bypass `zone.js` patch for IE/Edge
  */
 // (window as any).__Zone_enable_cross_context_check = true;
-
 
 /***************************************************************************************************
  * APPLICATION IMPORTS
