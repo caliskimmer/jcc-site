f31faebdf4a2e1ba95d6e532cc0e8ce9d21cdcf5
Matas <matasempakeris@gmail.com>
2020-03-29 18:42:52 -0700

    removed directives directory as none left

--- src/app/directives/add-background-on-scroll.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AddBackgroundOnScrollDirective } from './add-background-on-scroll.directive';
-
-describe('AddBackgroundOnScrollDirective', () => {
-  it('should create an instance', () => {
-    const directive = new AddBackgroundOnScrollDirective();
-    expect(directive).toBeTruthy();
-  });
-});
