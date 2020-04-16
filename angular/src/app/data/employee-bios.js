ef3998243754f21bd95dc34c44b769e69deec601
Matas <matasempakeris@gmail.com>
2020-04-15 22:51:53 -0700

    updated employee bios

--- angular/src/app/data/employee-bios.js
+++ angular/src/app/data/employee-bios.js
@@ -3,28 +3,18 @@ export default [
     name: 'Jasmine Johnson',
     photo: 'assets/employee-images/JasmineJohnson.jpg',
     bio:
-      'asdfjasd;lfkjasd;lfkjasd;lfkjas;dlfkja;sdlfkjsad;lfkj;alsdkfj;asdlkfj;als',
+      'Our Chiropractic Assistant Jasmine Johnson graduated from Southern California Health Institute in the San Fernando Valley.  Her interest in human anatomy and enhancing others’ quality of life lead to her interest in becoming a rehab tech/massage therapist. She has been in the healthcare industry for 8 years and is still amazed by everything she is continuing to learn. In her spare time she loves going to try new places to eat, read and watch movies. Don’t forget to ask her about THE BEST cheeseburgers in Los Angeles!',
   },
   {
     name: 'Liz Manzo',
     photo: 'assets/employee-images/LizManzo.jpg',
-    bio: '',
-  },
-  {
-    name: 'Kelly Ryans',
-    photo: 'assets/employee-images/KellyRyans.jpg',
-    bio: `
-            Hometown: Los Angeles, CA\n
-            Education:  Everest College\n
-            Favorite books: The Flight of Dragons, Legion of the Damned\n
-            Movies: Howl\'s Moving Castle, The Forbidden Planet\n
-            Music: Collective Soul, Air, A Perfect Circle\n
-            Interests: painting, animation
-        `,
+    bio:
+      'Our Office Manager Liz Manzo has been working in the healthcare industry for 16 years. Her role is to help guide patients through their experience from their first phone call through the end of their care plan. Whether it’s scheduling appointments to accommodate the patient’s busy schedule or creating reasonable payment options, her goal is to make the entire experience as seamless as possible. Liz graduated from Bryman College.  When not at Joga Chiropractic Center, Liz spends her off time with her 16 year old daughter Emily, hiking, camping and watching the newest shows on Netflix! She is sure to always have a smile for you at the front desk!',
   },
   {
     name: 'Shirra Leavitt',
     photo: 'assets/employee-images/ShirraLeavitt.jpg',
-    bio: '',
+    bio:
+      "Shirra Leavitt graduated from Syracuse University with a BFA.  Currently from Santa Monica, she is CAMTC certified (#14119) and an AMTA member. She has over 20 years of massage experience. She has always maintained an active and healthy lifestyle and believes massage is a great way to increase an individual's overall physical and emotional well-being. She is skilled in acupressure, neuromuscular trigger point therapy, polarity and cranial sacral work, as well as prenatal massage and energy work with tuning forks and crystals. She customizes each massage to meet her clients’ needs during each session and offers CBD infused massage as an add on.",
   },
 ];
