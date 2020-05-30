6646da02fb83bf2fe8d9ddee240165b70764673c
Matas <matasempakeris@gmail.com>
2020-05-30 13:55:46 -0700

    added phone number, minor change in style in import

--- angular/src/main.ts
+++ angular/src/main.ts
@@ -1,6 +1,6 @@
 import { AppModule } from '@modules/app/app.module';
 import { enableProdMode } from '@angular/core';
-import { environment } from './environments/environment.prod';
+import { environment } from '@environments/environment.prod';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 if (environment.production) {
